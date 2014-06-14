492e5efd207702b95592fc9a3be78659aa65b3e8
Harry Pan <gs0622@gmail.com>
2014-06-14 20:23:07 +0800

    min with O(1)

--- StackWithMin.c
+++ StackWithMin.c
@@ -1,3 +1,7 @@
+/*Problem: http://codercareer.blogspot.tw/2011/09/no-02-stack-with-function-min.html
+Define a stack, in which we can get its minimum number with a function min.
+In this stack, the time complexity of min(), push() and pop() are all O(1).
+*/
 #include <stdio.h>
 #include <stdlib.h>
 struct node {
@@ -5,28 +9,38 @@ struct node {
     struct node *next;
 };
 struct node *top = NULL;
+int min;
 void push(int v)
 {
     struct node *cur = malloc(sizeof(struct node));
+    if (!cur) return; else cur->next=NULL;
+    if (!top) min=v;
+    else if (min>v) {
+        int w = 2*v-min; /*less than v*/
+        min = v; /*new min by v*/
+        v = w; /*push w on stack top*/
+    }
     cur->data=v, cur->next=top, top=cur;
 }
 int pop(void)
 {
     int v;
     struct node *cur = top;
-    if (!top) return 0; /*TBD*/
+    if (!top) return -1; /*TBD*/
     v = cur->data, top = top->next, free(cur);
+    if (min>v) { int w=min; min=2*min-v, v=w; }
     return v;
 }
 void show(void)
 {
     struct node *cur = top;
+    if (!cur) return;
     printf("stack: ");
     while (cur) {
         printf("%d ", cur->data);
         cur = cur->next;
     }
-    printf("\n");
+    printf(" min: %d\n", min);
 }
 int main(void)
 {
@@ -34,8 +48,8 @@ int main(void)
     while (1) {
         printf("0. push 1.pop\n");
         if (scanf("%d", &i)!=1) break;
-        if (0==i) scanf("%d", &j), push(j);
-        if (1==i) printf("%d\n", pop());
+        if (0==i) printf("push? "), scanf("%d", &j), push(j);
+        if (1==i) printf("pop %d\n", pop());
         show();
     }
     return 0;
