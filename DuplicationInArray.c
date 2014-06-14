9a756efdf48353c6b17962a6de888d138047ae00
Harry Pan <gs0622@gmail.com>
2014-06-14 23:38:23 +0800

    revise detection by hash collision

--- DuplicationInArray.c
+++ DuplicationInArray.c
@@ -5,29 +5,21 @@ the array. How do you find the duplicated number? For example, if an array with 
 */
 #include <stdio.h>
 #include <stdlib.h>
-int duplicate(int *arr, int n)
-{
-    int i, sum=0, ref = ((n-2)*(n-1)/2);
-    for (i=0;i<n;i++) {
-        int v = arr[i];
-        if (v<0 || v>=n-1) {
-            printf("invalid %d\n", v);
-            return (-1)*i;
-        }
-        sum+=v;
-    }
-    return sum-ref;
-}
 int main(int argc, char **argv)
 {
-    int i, n, *arr, ret;
+    int i, n, v, *arr;
     if (argc == 1) return 0; else n = argc-1;
     arr = malloc(sizeof(int)*n);
-    for (i=0;i<n;i++) arr[i]=atoi(argv[i+1]);
-    for (i=0;i<n;i++) printf("%d ", arr[i]);
-    ret = duplicate(arr, n);
-    if (ret>=0) printf("\nduplicate=%d\n", ret);
-    else printf("\nerror in %d\n", arr[ret*(-1)]);
+    for (i=0;i<n;i++) arr[i]=-1;
+    for (i=0;i<n;i++) {
+        v = atoi(argv[i+1]);
+        if (v<0||v>n-2) {
+            printf("%d invalid\n", v);
+            break;
+        }
+        if (arr[v]==-1) arr[v]=v;
+        else printf("%d duplicated\n", v);
+    }
     free(arr);
     return 0;
 }
