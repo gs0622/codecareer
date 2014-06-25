6f235209c9c89ac957a5f5f8554539e2fd6f91f9
Harry Pan <gs0622@gmail.com>
2014-06-25 23:46:26 +0800

    add DP solution

--- MaxSumOfSubArrays.c
+++ MaxSumOfSubArrays.c
@@ -1,11 +1,26 @@
+/*test case: a.out -5 0 -4 1 4 3 -2*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#define MAX(a,b)    ((a)>(b)?(a):(b))
 int main(int argc, char **argv)
 {
+#if 1 /*DP*/
+    int i, max, data[100]={}, dp[100]={}, n=argc-1;
+    if (0==n) return 0;
+    assert(n<100);
+    for (i=1;i<argc;i++) data[i-1]=atoi(argv[i]);
+    //for (i=0;i<n;i++) printf("%3d ", data[i]); puts("");
+    dp[0]=MAX(0, data[0]);
+    for (i=1;i<n;i++) dp[i]=MAX(0, dp[i-1]+data[i]);
+    //for (i=0;i<n;i++) printf("%3d ", dp[i]);
+    for (i=1, max=dp[0];i<n;i++) max = MAX(dp[i], max);
+    printf("max=%d\n", max);
+#else /*Brute force*/
     int i, j, k, sum=0, max=0, n = argc-1, data[100];
     if (!n) return 0;
     for (i=1;i<argc;i++) data[i-1]=atoi(argv[i]);
-    for (i=0;i<n;i++) printf("%d ", data[i]);
+    //for (i=0;i<n;i++) printf("%d ", data[i]); puts("");
     printf("\n");
     for (i=0;i<n;i++) {
         for (j=1;j<=n;j++) {
@@ -17,5 +32,6 @@ int main(int argc, char **argv)
         }
     }
     printf("max=%d\n", max);
+#endif
     return 0;
 }
