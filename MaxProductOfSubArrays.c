529399c20d3d3c803a331e354acfc35bbf7375b0
harrypan <gs0622@gmail.com>
2014-07-06 00:49:36 +0800

    add DP version draft, need more tests

--- MaxProductOfSubArrays.c
+++ MaxProductOfSubArrays.c
@@ -5,7 +5,31 @@
 #include <stdlib.h>
 #include <assert.h>
 #define MAX(a,b)    ((a)>(b)?(a):(b))
-#if 1
+#define MIN(a,b)    ((a)<(b)?(a):(b))
+#if 1 /*DP version, time: O(n)*/
+int main(int argc, char **argv)
+{
+    int i, max, min, neg, cnt, mul, n = argc-1, data[100];
+    if (!n) return 0;
+    for (i=1;i<argc;i++) data[i-1]=atoi(argv[i]);
+    for (i=0,max=data[0],min=data[0],mul=1,cnt=0;i<n;i++) {
+        if (0==data[i]) {mul=1,cnt=0; continue;}
+        neg = (data[i]<0)? 1: 0; /*is negative*/
+        cnt += neg? 1: 0; /*total neg count*/
+        mul *= data[i];
+        if (0==neg) { /*positive*/
+            max = (0==cnt%2)? MAX(max, mul): MAX(max, data[i]);
+            min = (0==cnt%2)? MIN(min, mul): MIN(min, data[i]);
+        } else { /*negative*/
+            max = (0==cnt%2)? MAX(max, mul): MAX(max, data[i]);
+            min = (0==cnt%2)? MIN(min, mul): MIN(min, data[i]);
+        }
+    }
+    printf("max=%d\n", max);
+    return 0;
+}
+#endif
+#if 0 /*Brute force, time: O(n^2)*/
 int main(int argc, char **argv)
 {
     int i, j, max, mul, n = argc-1, data[100];
@@ -21,10 +45,10 @@ int main(int argc, char **argv)
     printf("max=%d\n", max);
     return 0;
 }
-#else
+#endif
+#if 0 /*Brute force, time: O(n^3)*/
 int main(int argc, char **argv)
 {
-    /*Brute force, time: O(n^3)*/
     int i, j, k, mul=1, max=0, n = argc-1, data[100];
     if (!n) return 0;
     for (i=1;i<argc;i++) data[i-1]=atoi(argv[i]);
