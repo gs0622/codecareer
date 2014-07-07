caefbb635abbeea41963efaf02094e8b60b22398
gs0622 <gs0622@users.noreply.github.com>
2014-07-07 19:18:44 +0800

    Update MaxProductOfSubArrays_HP.c

--- MaxProductOfSubArrays_HP.c
+++ MaxProductOfSubArrays_HP.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX(a,b)    ((a)>(b)?(a):(b))
-int MaxProductSeq(const int arr[], int sz)
+#define MAX(a,b)    ((a)>(b)?(a):(b))long MaxProductSeq(const long arr[], long sz)
 {
-    int i, max, neg, cnt, mul, tmp;
+    long i, max, neg, cnt, mul, tmp;
     if (sz<=0) return 0;
     for (i=0,max=arr[0],mul=1,cnt=0,tmp=0;i<sz;i++) {
         if (0==arr[i]) { mul=1,cnt=0; continue; }
@@ -18,9 +17,9 @@ int MaxProductSeq(const int arr[], int sz)
 }
 int main(int argc, char **argv)
 {
-    int i, n = argc-1, data[100];
-    for (i=1;i<argc;i++) data[i-1]=atoi(argv[i]);
-    printf("max=%d\n", MaxProductSeq(data, n));
+    long i, n = argc-1, data[100];
+    for (i=1;i<argc;i++) data[i-1]=atol(argv[i]);
+    printf("max=%ld\n", MaxProductSeq(data, n));
     return 0;
 }
 
